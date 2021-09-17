1b471498389795f9a29c4af96f5621e5c90bc6b2
SergioGrandeH <sergio.grande.her@gmail.com>
2021-09-17 17:46:44 +0200

    Ejercicio 1

--- Ejercicios/Assets/NewBehaviourScript.cs
+++ Ejercicios/Assets/NewBehaviourScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    // Tipos de variables//
+
+    public string mensaje = "Buenas tardes";
+    public float numerodecimal = 1.2f ;
+    public int numeroentero = 12;
+    public bool vivo = true;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        print(mensaje);
+        print(numerodecimal);
+        print(numeroentero);
+        print(vivo);
+
+        print(numeroentero + numerodecimal);
+        }
 
     // Update is called once per frame
     void Update()
     {
-        
+        print(numeroentero + numerodecimal);
     }
 }
