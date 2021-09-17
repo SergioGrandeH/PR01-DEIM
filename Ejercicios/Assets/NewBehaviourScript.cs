323c5aa330298573eea15cd67a18cd1f62806723
SergioGrandeH <sergio.grande.her@gmail.com>
2021-09-17 20:06:04 +0200

    Ej2.2y2.3

--- Ejercicios/Assets/NewBehaviourScript.cs
+++ Ejercicios/Assets/NewBehaviourScript.cs
@@ -11,23 +11,35 @@ public class NewBehaviourScript : MonoBehaviour
     public int numeroentero = 12;
     public bool vivo = true;
     int n = 0;
+    int l = 10;
+    int m = 20;
 
     // Start is called before the first frame update
     void Start()
     {
-       
-        
 
+        while (n < 20)
+        {
+            n++;
+            print(n);   
+        if (n == 20)
+            {
+                print(mensaje);
+            }
+        }
+        for (m = 20; m <= 30; m++)
+        { 
+            print(m); 
+            if(m == 30)
+            {
+                print(mensaje);
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        n++;
-        if (n < 10)
-        {
-            print(n);
-        }
-        
+    
     }
 }
