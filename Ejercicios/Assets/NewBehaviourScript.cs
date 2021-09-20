421febefc23327b46261a336a0f78505c725fb0b
SergioGrandeH <sergio.grande.her@gmail.com>
2021-09-20 18:35:11 +0200

    Ejercicio 2.c

--- Ejercicios/Assets/NewBehaviourScript.cs
+++ Ejercicios/Assets/NewBehaviourScript.cs
@@ -10,17 +10,24 @@ public class NewBehaviourScript : MonoBehaviour
     public float numerodecimal = 1.2f ;
     public int numeroentero = 12;
     public bool vivo = true;
-    int n = 0;
+   
     int l = 10;
     int m = 20;
 
     // Start is called before the first frame update
     void Start()
     {
-        for (int n = 0; n < 5; n++)
+
+        for (int n = 0; n < 10; n++)
         {
-             print(n); 
+            print(n);
+            if (n > 3)
+
+            {
+                print(mensaje);
+            }
         }
+         
     }
 
     // Update is called once per frame
