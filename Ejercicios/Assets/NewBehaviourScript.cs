7a59d236a93767dbb0bdb71994fe3f1c13e24a2e
Sergio Grande <sergio.grande.her@gmail.com>
2021-09-24 17:46:30 +0200

    Ej 3.c

--- Ejercicios/Assets/NewBehaviourScript.cs
+++ Ejercicios/Assets/NewBehaviourScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NewBehaviourScript : MonoBehaviour
 {
@@ -10,27 +11,53 @@ public class NewBehaviourScript : MonoBehaviour
     public float numerodecimal = 1.2f ;
     public int numeroentero = 12;
     public bool vivo = true;
-    int num1 = 5;
-    int num2 = 10;
-    int m = 20;
+    int x = 0;
+    int y = 0;
+    int z = 0;
     int Multiplicar; 
     int multiplicar (int num1, int num2)
         {
             int resultado = num1 * num2;
             return resultado;
         }
-        
-   
+    public Text timer;
+
+    IEnumerator contador()
+    {
+        while (true)
+        {
+            print("La hora es " + z + ":" + y + ":" + x);
+
+            timer.text = "La hora es" + z + ":" + y + ":" + x;
+            x++;
+            yield return new WaitForSeconds(1f);
+            if (x == 60)
+            {
+                x = 0;
+                y++;
+            }
+            if (y == 60)
+            {
+                y = 0;
+                z++;
+            }
+        }
+    }
+
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        int resultado = multiplicar(5, 10);
-        print(resultado);
-        if (resultado != 60)
-        {
-            bool vivo = false;
-            print (vivo);
-                }
+        
+        StartCoroutine("contador");
+       
 
     }
     
