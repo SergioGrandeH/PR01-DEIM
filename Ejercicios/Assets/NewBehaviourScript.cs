51e854dc70cfce9cb907e8a2d4ff876d11036075
Sergio Grande <sergio.grande.her@gmail.com>
2021-09-24 17:04:56 +0200

    Ej 3.1

--- Ejercicios/Assets/NewBehaviourScript.cs
+++ Ejercicios/Assets/NewBehaviourScript.cs
@@ -10,8 +10,8 @@ public class NewBehaviourScript : MonoBehaviour
     public float numerodecimal = 1.2f ;
     public int numeroentero = 12;
     public bool vivo = true;
-    int num1 = 50;
-    int num2 = 15;
+    int num1 = 5;
+    int num2 = 10;
     int m = 20;
     int Multiplicar; 
     int multiplicar (int num1, int num2)
@@ -19,13 +19,14 @@ public class NewBehaviourScript : MonoBehaviour
             int resultado = num1 * num2;
             return resultado;
         }
-         
-
+        void Saludo ( string Hola)
+    {
+        print("hola" + Hola);
+    }
     // Start is called before the first frame update
     void Start()
     {
-        int res = multiplicar(50, 15);
-        print(res);
+        Saludo("Juan");
     }
     
     // Update is called once per frame
